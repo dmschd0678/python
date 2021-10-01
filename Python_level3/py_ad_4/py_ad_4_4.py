88aa4c9d39c78e6c04447e858b9cacf5448a442c
dmschd0678@naver.com <63108606+dmschd0678@users.noreply.github.com>
2021-10-01 23:58:16 +0900

    PyPI 배포!

--- Python_level3/py_ad_4/py_ad_4_4.py
+++ Python_level3/py_ad_4/py_ad_4_4.py
@@ -17,5 +17,24 @@ c.convert_gif()
 패키지 배포 순서(PyPI)
 
 1. https://pypi.org 회원가입
+2. 프로젝트 구조 확인
+3. __init__.py 작성
+4. 프로젝트 Root 필수 파일 작성
+    - README.md
+    - gitignore
+    - setup.py
+    - setup.cfg(optional)
+    - LICENSE
+    - MANIFEST.in
+5. pip install setuptools, wheel 설피 후 빌드 업 -> 설치판 생성
+    -> 설치1 : python -m pip install --upgrade setuptools wheel
+    -> 설치2 : python -m pip install --user --upgrade setuptools wheel
+    -> 빌드 : python setup.py sdist bdist wheel
 
+6. PyPI 배포
+    -> 설피 : pip install twine
+    -> 업로드 : python -m twine upload dist/*
+
+7. 설치 확인(pip install python_gifConverter)
+    from python_gifConverter.converter import GifConverter as Alias
 """
