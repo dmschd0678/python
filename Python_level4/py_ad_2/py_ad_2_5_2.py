3e2c812443e152f53be778151d5449298ce270e9
dmschd0678@naver.com <63108606+dmschd0678@users.noreply.github.com>
2021-10-17 23:26:15 +0900

    multiprocessing - 4
    Process Memory Sharing
    Share Value
    Share Array

---
+++ Python_level4/py_ad_2/py_ad_2_5_2.py
@@ -0,0 +1,51 @@
+"""
+Section 2
+Parallelism with Multiprocessing - multiprocessing(4) - Sharing state
+Keyword - memory sharing, array, value
+"""
+
+from multiprocessing import Process, current_process,Value,Array
+import os
+
+# 프로세스 메모리 공유 예제(공유 O)
+
+# 실행 함수
+def generate_update_number(v: int): # int형 변수
+    for _ in range(50):
+        v.value+= 1
+    print(current_process().name,"data",v.value)
+
+def main():
+    # 부모 프로세스 아이디
+    parent_process_id = os.getpid()
+    # 출력
+    print(f'Parent Process ID : {parent_process_id}')
+
+    # 프로세스 리스트 선언
+    processes = []
+
+    # 프로세스 메모리 공유 변수
+    # from multiprocess import shared_memory 사용 가능(파이썬 3.0)
+    # from multiprocess import Manager 사용 가능
+
+    share_Array = Array('i',range(50))
+    share_value = Value('i',0)  # 변수형 엄격 : i = int, f = float, c = char
+
+    for _ in range(1,10):
+        # 생성
+        p = Process(target=generate_update_number, args=(share_value,))
+
+        # 배열에 담기
+        processes.append(p)
+        # 실행
+        p.start()
+
+    # join
+    for p in processes:
+        p.join()
+
+    # 최종 프로세스 부모 변수 확인
+    print('Final Data in parent process',share_value)
+
+if __name__ == "__main__":
+    main()
